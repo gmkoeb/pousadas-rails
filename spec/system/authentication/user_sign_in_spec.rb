b55579921666ab456d525bf5c167e2b3ab1f3a8d
Gabriel Manika Koeb <gmkoeb@gmail.com>
2023-10-31 13:10:03 -0300

    Incluir testes para login de usuários

---
+++ spec/system/authentication/user_sign_in_spec.rb
@@ -0,0 +1,81 @@
+require 'rails_helper'
+
+describe 'Usuário padrão se autentica' do
+  it 'com sucesso' do
+    # Arrange
+    User.create!(email: 'gmkoeb@gmail.com', password: 'password')
+    # Act
+    visit root_path
+    click_on 'Entrar'
+    fill_in 'E-mail', with: 'gmkoeb@gmail.com'
+    fill_in 'Senha', with: 'password'
+    within '.actions' do
+      click_on 'Entrar'
+    end
+    # Assert
+    expect(page).to have_content 'Login efetuado com sucesso.'
+    expect(page).not_to have_link 'Entrar'
+    expect(page).not_to have_link 'Cadastrar pousada'
+    expect(page).to have_button 'Sair'
+    within 'nav' do
+      expect(page).to have_content 'gmkoeb@gmail.com'
+    end
+  end
+
+  it 'e realiza log out' do
+    # Arrange
+    User.create!(email: 'gmkoeb@gmail.com', password: 'password')
+    # Act
+    visit root_path
+    click_on 'Entrar'
+    fill_in 'E-mail', with: 'gmkoeb@gmail.com'
+    fill_in 'Senha', with: 'password'
+    within '.actions' do
+      click_on 'Entrar'
+    end
+    click_on 'Sair'
+    # Assert
+    expect(page).to have_content 'Logout efetuado com sucesso.'
+    expect(page).to have_link 'Entrar'
+    expect(page).not_to have_content 'gmkoeb@gmail.com'
+    expect(page).not_to have_button 'Sair'
+  end
+
+  it 'com informações incorretas' do
+    # Arrange
+    User.create!(email: 'gmkoeb@gmail.com', password: 'password')
+    # Act
+    visit root_path
+    click_on 'Entrar'
+    fill_in 'E-mail', with: 'gmkoeb@gmail.com'
+    fill_in 'Senha', with: 'password123'
+    within '.actions' do
+      click_on 'Entrar'
+    end
+    # Assert
+    expect(page).to have_content 'E-mail ou senha inválidos.'
+  end
+end
+
+describe 'Usuário admin se autentica' do
+  it 'com sucesso' do
+    # Arrange
+    User.create!(email: 'gmkoeb@gmail.com', password: 'password', admin: 'true')
+    # Act
+    visit root_path
+    click_on 'Entrar'
+    fill_in 'E-mail', with: 'gmkoeb@gmail.com'
+    fill_in 'Senha', with: 'password'
+    within '.actions' do
+      click_on 'Entrar'
+    end
+    # Assert
+    expect(page).to have_content 'Login efetuado com sucesso.'
+    expect(page).not_to have_link 'Entrar'
+    expect(page).to have_link 'Cadastrar pousada'
+    expect(page).to have_button 'Sair'
+    within 'nav' do
+      expect(page).to have_content 'gmkoeb@gmail.com'
+    end
+  end
+end
