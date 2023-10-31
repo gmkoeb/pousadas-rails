ff40945d38cadf99ea7ad18643275863758735a9
Gabriel Manika Koeb <gmkoeb@gmail.com>
2023-10-31 18:08:09 -0300

    Incluir formas de pagamento via checkbox na view

--- app/controllers/inns_controller.rb
+++ app/controllers/inns_controller.rb
@@ -1,8 +1,10 @@
 class InnsController < ApplicationController
   def new
     @inn = Inn.new
+    @payment_methods = ["Dinheiro", "PIX", "Cartão de crédito", "Cartão de débito"]
   end
 
   def create
+    pp params
   end
 end
