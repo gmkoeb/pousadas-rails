d4e9528c1d7ef6b76728cac753bfc70994ab0252
Gabriel Manika Koeb <gmkoeb@gmail.com>
2023-11-01 18:01:42 -0300

    Incluir botão de home para melhorar navegação de usuários

--- spec/system/user_view_homepage_spec.rb
+++ spec/system/user_view_homepage_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe 'Usuário visita página inicial' do
+describe 'Usuário não autenticado visita página inicial' do
   it 'e vê a logo da aplicação' do
     # Arrange
 
@@ -8,5 +8,18 @@ describe 'Usuário visita página inicial' do
     visit root_path
     # Assert
     expect(page).to have_content 'Pousadas Rails'
+    expect(page).to_not have_content 'Home'
+  end
+
+  it 'e vê o botão de entrar' do
+    # Arrange
+
+    # Act
+    visit root_path
+    # Assert
+    within 'nav' do
+      expect(page).to have_content 'Entrar'
+    end
+    expect(page).to_not have_content 'Home'
   end
 end
