de8aadc4c00a9ff3727704301d50a6a80605230a
Gabriel Manika Koeb <gmkoeb@gmail.com>
2023-11-07 11:10:25 -0300

    Incluir feature de exibição de pousadas mais recentes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,6 @@ class HomeController < ApplicationController
   def index
     admin_has_inn?
     @inns = Inn.published
+    @recent_inns = Inn.published.order(created_at: :desc).limit(3)
   end
 end
