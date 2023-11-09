dc9bf07c6baa1b2f03cd74e670cdfd5b33c3ea82
Gabriel Manika Koeb <gmkoeb@gmail.com>
2023-11-09 11:50:51 -0300

    Alterar chamada do método admin_has_inn? no home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
+  before_action :admin_has_inn?
   def index
-    admin_has_inn?
     @inns = Inn.published
     @recent_inns = Inn.published.order(created_at: :desc).limit(3)
     @older_inns = Inn.published.excluding!(@recent_inns)
